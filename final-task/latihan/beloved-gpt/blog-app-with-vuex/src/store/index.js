21e00b69f30e2f38f06dda66bd105b7f2e56b8c7
bayufadayan <bayufadayan@gmail.com>
2025-10-28 11:23:25 +0700

    add store and setup vuex state management

---
+++ final-task/latihan/beloved-gpt/blog-app-with-vuex/src/store/index.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        posts: []
+    },
+    mutations: {
+        setPosts(state, posts) {
+            state.posts = posts;
+        },
+        addPost(state, post) {
+            state.posts.push(post);
+        },
+        deletePost(state, index) {
+            state.posts.splice(index, 1);
+        }
+    },
+    actions: {
+        loadPosts({ commit }) {
+            const saved = JSON.parse(localStorage.getItem("posts")) || [];
+            commit("setPosts", saved);
+        },
+        savedPosts({ state }) {
+            localStorage.setItem("posts", JSON.stringify(state.posts));
+        },
+        addNewPost({ commit, dispatch }, post) {
+            commit("addPost", post);
+            dispatch("savePosts");
+        },
+        removePost({ commit, dispatch }, index) {
+            commit("deletePost", index);
+            dispatch("savePosts");
+        }
+    },
+    getters: {
+        posts: state => state.posts
+    }
+});
